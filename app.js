506b1f1ffbceb70eedd7a0ac8b3866203b20d6ac
Karl McGuire <karl@xba.io>
2019-10-15 00:41:13 -0400

    idk

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 const express = require("express")
-const ejs = require("ejs")
 const app = express()
+
+app.set("view engine", "ejs")
 
 app.use("/css", express.static("css"))
 app.use("/js", express.static("js"))
 
 app.get("/", (req, res) => {
-  ejs.renderFile("index.ejs", {}, null, (err, str) => {
-    if(err != null) {
-      console.log(err)
-    }
-    res.send(str) 
-  }) 
+  res.render("index")
 })
 
-app.get("/about/", (req, res) => {})
-app.get("/contact/", (req, res) => {})
-app.get("/connections/", (req, res) => {})
-app.get("/newConnection/", (req, res) => {})
-app.get("/savedConnections/", (req, res) => {})
+app.get("/about", (req, res) => {
+  res.render("about")
+})
+
+app.get("/contact", (req, res) => {
+  res.render("contact") 
+})
+
+app.get("/connections", (req, res) => {
+  res.render("connections")
+})
+
+app.get("/connection", (req, res) => {
+  res.render("connection")
+})
+
+app.get("/newConnection", (req, res) => {
+  res.render("newConnection")
+})
+
+app.get("/savedConnections", (req, res) => {
+  res.render("savedConnections")
+})
 
 app.listen(8000, () => console.log("listening on :8000"))
